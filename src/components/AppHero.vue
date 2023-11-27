c5c0fbd487f30455865690614cf79535885bfacf
PinoGabriel <pinogabriel01@gmail.com>
2023-11-27 18:06:55 +0100

    feat: added Jumbo

---
+++ src/components/AppHero.vue
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<template>
+  <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--XNA9f4OE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/i0tgmgk76qyfha15i4qv.png" alt="">
+</template>
+
+<style scoped>
+</style>
